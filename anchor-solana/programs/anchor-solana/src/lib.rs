1168ceee535e3e2922efba5fdf9ba72aeb7ed1da
MediconcenVictor <victor_chang@mediconcen.com>
2024-03-26 23:23:40 +0800

    anchor init

---
+++ anchor-solana/programs/anchor-solana/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
+use solana_program::system_instruction;
+declare_id!("11111111111111111111111111111111");
+
+#[derive(Accounts)]
+pub struct TransferLamports<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[program]
+pub mod solana_lamport_transfer {
+    use super::*;
+    pub fn transfer_lamports(ctx: Context<TransferLamports>, amount: u64) -> Result<()> {
+        let from_account = &ctx.accounts.from;
+        let to_account = &ctx.accounts.to;
+        // Create the transfer instruction
+        let transfer_instruction =
+            system_instruction::transfer(from_account.key, to_account.key, amount);
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                from_account.to_account_info(),
+                to_account.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+        Ok(())
+    }
+}
