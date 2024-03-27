e30bbe36644d783b1bd985d7fdf5462ebad069a7
MediconcenVictor <victor_chang@mediconcen.com>
2024-03-27 23:23:35 +0800

    fix build failed

--- anchor-solana/programs/anchor-solana/src/lib.rs
+++ anchor-solana/programs/anchor-solana/src/lib.rs
@@ -8,6 +8,7 @@ pub struct TransferLamports<'info> {
     #[account(mut)]
     pub from: Signer<'info>,
     #[account(mut)]
+    /// CHECK: Requires careful handling to prevent unintended behavior.
     pub to: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
@@ -34,3 +35,47 @@ pub mod solana_lamport_transfer {
         Ok(())
     }
 }
+
+// #[derive(Accounts)]
+// pub struct TransferLamportsSigned<'info> {
+//     #[account(mut, signer)]
+//     ///CHECK:
+//     pub from: AccountInfo<'info>,
+//     #[account(mut)]
+//     ///CHECK:
+//     pub to: AccountInfo<'info>,
+//     ///CHECK:
+//     pub authority: AccountInfo<'info>,
+//     ///CHECK:
+//     pub system_program: AccountInfo<'info>,
+// }
+// pub mod solana_lamport_transfer {
+//     use super::*;
+
+//     pub fn transfer_lamports_signed(
+//         ctx: Context<TransferLamportsSigned>,
+//         amount: u64,
+//     ) -> Result<()> {
+//         let from_account = &ctx.accounts.from;
+//         let to_account = &ctx.accounts.to;
+//         let authority_account = &ctx.accounts.authority;
+
+//         // Create the transfer instruction
+//         let transfer_instruction =
+//             system_instruction::transfer(from_account.key, to_account.key, amount);
+
+//         // Invoke the transfer instruction signed by the authority
+//         anchor_lang::solana_program::program::invoke_signed(
+//             &transfer_instruction,
+//             &[
+//                 from_account.clone(),
+//                 to_account.clone(),
+//                 authority_account.clone(),
+//                 ctx.accounts.system_program.clone(),
+//             ],
+//             &[&[authority_account.key().as_ref()]],
+//         )?;
+
+//         Ok(())
+//     }
+// }
